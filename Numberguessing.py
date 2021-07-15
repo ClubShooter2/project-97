7d719c58e2616eb4e61402a5defcd80aa27f123c
ClubShooter2 <67182844+ClubShooter2@users.noreply.github.com>
2021-07-15 05:37:41 -0700

    Add files via upload

---
+++ Numberguessing.py
@@ -0,0 +1,14 @@
+import random
+
+value = random.randint(1,9)
+randomnumber = input("Enter any random number of 1-9:")
+
+
+while chances < 5
+
+if not chances < 5:
+     print("You lose Better luck next time")
+
+   
+    elif randomnumber == random(1,9):
+        print("Congratulations you won")
